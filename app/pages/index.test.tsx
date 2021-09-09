86b95d0aabfb54d5285d2671f624a6f36b1ba247
nicholasdieke <nicholasdieke@icloud.com>
2021-09-09 13:03:08 +0200

    added render.yaml file

--- app/pages/index.test.tsx
+++ app/pages/index.test.tsx
@@ -1,7 +1,6 @@
-import { render } from "test/utils"
-
-import Home from "./index"
 import { useCurrentUser } from "app/core/hooks/useCurrentUser"
+import { render } from "test/utils"
+import Home from "./index"
 
 jest.mock("app/core/hooks/useCurrentUser")
 const mockUseCurrentUser = useCurrentUser as jest.MockedFunction<typeof useCurrentUser>
@@ -13,7 +12,7 @@ test.skip("renders blitz documentation link", () => {
 
   // This is an example on how to mock api hooks when testing
   mockUseCurrentUser.mockReturnValue({
-    id: 1,
+    id: "1",
     name: "User",
     email: "user@email.com",
     role: "user",
